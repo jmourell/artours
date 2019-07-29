b0c46b26352b5e6f063996424257041307c09a28
Julian Mourell <j.mourell@gmail.com>
2019-07-29 11:39:56 +0900

    Add readme

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
       host: process.env.DB_HOST || "127.0.0.1",
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME || "artours",
-      user: process.env.DB_USER || "postgres",
-      password: process.env.DB_PASSWORD || "postgres",
+      user: process.env.DB_USER || "melvin",
+      password: process.env.DB_PASSWORD || "melvin",
     },
   },
 
